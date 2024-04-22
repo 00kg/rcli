a3b45e7f403d25beea0cd2304e048ef55689e430
work <work@debian-pc.debian-pc.com>
2024-04-22 03:08:51 -0400

    feature: add tower-http support

--- src/process/http_serve.rs
+++ src/process/http_serve.rs
@@ -9,6 +9,8 @@ use axum::{
 };
 use tracing::{info, warn};
 
+use tower_http::services::ServeDir;
+
 #[derive(Debug)]
 struct HttpServeState {
     path: PathBuf,
@@ -18,10 +20,18 @@ pub async fn process_http_serve(path: PathBuf, port: u16) -> Result<()> {
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
     info!("Serving {:?} on addr {}", path, addr);
 
-    let state = HttpServeState { path };
+    let state = HttpServeState { path: path.clone() };
+
+    let dir_service = ServeDir::new(path)
+        .append_index_html_on_directories(true)
+        .precompressed_gzip()
+        .precompressed_br()
+        .precompressed_zstd()
+        .precompressed_deflate();
 
     let router = Router::new()
         .route("/*path", get(file_handler))
+        .nest_service("/tower", dir_service)
         .with_state(Arc::new(state));
 
     // let addr = SocketAddr::from(([0,0,0,0],port));
@@ -47,6 +57,8 @@ async fn file_handler(
             format!("File {} not found", p.display()),
         )
     } else {
+        // TODO: test p is a directory
+        // if it is a directory, list all files/subdirectories as <li><a href="">
         match tokio::fs::read_to_string(p).await {
             Ok(content) => (StatusCode::OK, content),
             Err(e) => {
@@ -54,5 +66,21 @@ async fn file_handler(
                 (StatusCode::INTERNAL_SERVER_ERROR, e.to_string())
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_file_handler() {
+        let state = Arc::new(HttpServeState {
+            path: PathBuf::from("."),
+        });
+        let (status, content) =
+            file_handler(axum::extract::State(state), Path("Cargo.toml".to_string())).await;
+        assert_eq!(status, StatusCode::OK);
+        assert!(content.contains("anyhow"));
     }
 }
